0d072a3e5251bfdffd029059cec024c4ffcbf9ce
Sonja <mascarponee@gmail.com>
2017-02-17 14:43:07 +0100

    added code for suites

--- conf.js
+++ conf.js
@@ -9,14 +9,15 @@ exports.config = {
     framework: 'jasmine',
     // Spec patterns are relative to the current working directory when
     // protractor is called.
-    specs: ['firstTest.spec.js'],
+    specs: ['Test/**/*.spec.js'],
+
+    suites:{
+        smoke: 'Test/smoke/*.spec.js',
+    },
+    
     // Options to be passed to Jasmine.
     jasmineNodeOpts: {
         showColors: true,
         defaultTimeoutInterval: 30000
-    },
-    onPrepare: function(){
-    browser.driver.manage().window().setPosition(0,0);
-    browser.driver.manage().window().setSize(1280, 720);
-  }
+    }
 };
