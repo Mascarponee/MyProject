800d6ca449000dd11c455602882526d37ced0c22
Sonja <mascarponee@gmail.com>
2017-03-13 15:40:48 +0100

    Changed description/Refactoring

--- Test/Smoke/firstTest.spec.js
+++ Test/Smoke/firstTest.spec.js
@@ -1,17 +1,8 @@
 "use strict";
 var protractor_1 = require("protractor");
-var CreatePage = require('./Pages/firstTest.page.js');
-describe('Test the protractor demo', function () {
-    var resultTimestampfirstRow = protractor_1.element(protractor_1.by.repeater('result in memory').row(0).column('result.timestamp'));
-    var resultTimestampsecondRow = protractor_1.element(protractor_1.by.repeater('result in memory').row(1).column('result.timestamp'));
-    // let resultFirstfirstRow = element(by.repeater('result in memory').row(0).column('result.first'));
-    var resultFirstsecondRow = protractor_1.element(protractor_1.by.repeater('result in memory').row(1).column('result.first'));
-    var resultOperatorfirstRow = protractor_1.element(protractor_1.by.repeater('result in memory').row(0).column('result.operator'));
-    var resultOperatorsecondRow = protractor_1.element(protractor_1.by.repeater('result in memory').row(1).column('result.operator'));
-    var resultSecondfirstRow = protractor_1.element(protractor_1.by.repeater('result in memory').row(0).column('result.second'));
-    var resultsSecondsecondRow = protractor_1.element(protractor_1.by.repeater('result in memory').row(1).column('result.second'));
-    var resultValuefirstRow = protractor_1.element(protractor_1.by.repeater('result in memory').row(0).column('result.value'));
-    var resultValuesecondRow = protractor_1.element(protractor_1.by.repeater('result in memory').row(1).column('result.value'));
+var CreatePage = require("./Pages/firstTest.page.js");
+var createPage = new CreatePage();
+describe('Test the protractor Calculator', function () {
     beforeEach(function () {
         protractor_1.browser.get("http://juliemr.github.io/protractor-demo/");
     });
@@ -19,95 +10,96 @@ describe('Test the protractor demo', function () {
         console.log('Test finished');
     });
     describe('When it enters the page', function () {
-        xit('should be the correct page', function () {
+        it('it should be the correct page', function () {
             expect(protractor_1.browser.getCurrentUrl()).toBe('http://juliemr.github.io/protractor-demo/');
         });
-        xit('should check the title', function () {
+        it('it should check the title', function () {
             expect(protractor_1.browser.getTitle()).toBe('Super Calculator');
         });
     });
-    describe('When testing the calculator', function () {
+    describe('When calculating', function () {
         describe('When Using single calculations', function () {
-            it('should add two numbers, use addition and check the result value of the first line', function () {
-                CreatePage.enterFirstFieldValue('5');
-                CreatePage.chooseAddition();
-                CreatePage.enterSecondFieldValue('3');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('8'));
+            it('it should add two numbers, use addition and check the result value of the first line', function () {
+                createPage.enterFirstFieldValue('5');
+                createPage.chooseAddition();
+                createPage.enterSecondFieldValue('3');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('8');
             });
-            xit('should add two numbers,use division and check the result value of the first line', function () {
-                CreatePage.enterFirstFieldValue('10');
-                CreatePage.chooseDivision();
-                CreatePage.enterSecondFieldValue('2');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('5'));
+            it('it should add two numbers,use division and check the result value of the first line', function () {
+                createPage.enterFirstFieldValue('10');
+                createPage.chooseDivision();
+                createPage.enterSecondFieldValue('2');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('5');
             });
-            xit('should add two numbers,use multiplication and check the result value of the first line', function () {
-                CreatePage.enterFirstFieldValue('25');
-                CreatePage.chooseMultiplication();
-                CreatePage.enterSecondFieldValue('7');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('125'));
+            it('it should add two numbers,use multiplication and check the result value of the first line', function () {
+                createPage.enterFirstFieldValue('25');
+                createPage.chooseMultiplication();
+                createPage.enterSecondFieldValue('7');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('175');
             });
-            xit('should add two numbers,use subtraction and check the result value of the first line', function () {
-                CreatePage.enterFirstFieldValue('0');
-                CreatePage.chooseSubstraction();
-                CreatePage.enterSecondFieldValue('10');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('-10'));
+            it('it should add two numbers,use subtraction and check the result value of the first line', function () {
+                createPage.enterFirstFieldValue('0');
+                createPage.chooseSubstraction();
+                createPage.enterSecondFieldValue('10');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('-10');
             });
-            xit('should add two numbers,use modulo and check the result value of the first line', function () {
-                CreatePage.enterFirstFieldValue('5000');
-                CreatePage.chooseModulo();
-                CreatePage.enterSecondFieldValue('80');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('40'));
+            it('it should add two numbers,use modulo and check the result value of the first line', function () {
+                createPage.enterFirstFieldValue('5000');
+                createPage.chooseModulo();
+                createPage.enterSecondFieldValue('80');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('40');
             });
         });
         describe('When using multiple calculations', function () {
-            xit('should do different operations in one session and check the result for the latest', function () {
-                CreatePage.enterFirstFieldValue('5');
-                CreatePage.chooseAddition();
-                CreatePage.enterSecondFieldValue('3');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('8'));
-                CreatePage.enterFirstFieldValue('10');
-                CreatePage.chooseDivision();
-                CreatePage.enterSecondFieldValue('2');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('5'));
-                CreatePage.enterFirstFieldValue('10');
-                CreatePage.chooseMultiplication();
-                CreatePage.enterSecondFieldValue('2');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('20'));
+            it('it should do different operations in one session and check the result for the latest', function () {
+                createPage.enterFirstFieldValue('5');
+                createPage.chooseAddition();
+                createPage.enterSecondFieldValue('3');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('8');
+                createPage.enterFirstFieldValue('10');
+                createPage.chooseDivision();
+                createPage.enterSecondFieldValue('2');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('5');
+                createPage.enterFirstFieldValue('10');
+                createPage.chooseMultiplication();
+                createPage.enterSecondFieldValue('2');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('20');
             });
         });
-        xit('should test values from  ngRepeat', function () {
-            CreatePage.enterFirstFieldValue('5');
-            CreatePage.chooseAddition();
-            CreatePage.enterSecondFieldValue('3');
-            CreatePage.clickGO();
-            expect(CreatePage.getLatest().toEqual('8'));
-            CreatePage.enterFirstFieldValue('10');
-            CreatePage.chooseDivision();
-            CreatePage.enterSecondFieldValue('2');
-            CreatePage.clickGO();
-            expect(CreatePage.getLatest().toEqual('5'));
-            resultTimestampfirstRow.getText().then(function (args) {
+        it('it should test values from  ngRepeat', function () {
+            createPage.enterFirstFieldValue('5');
+            createPage.chooseAddition();
+            createPage.enterSecondFieldValue('3');
+            createPage.clickGO();
+            expect(createPage.getLatest()).toEqual('8');
+            createPage.enterFirstFieldValue('10');
+            createPage.chooseDivision();
+            createPage.enterSecondFieldValue('2');
+            createPage.clickGO();
+            expect(createPage.getLatest()).toEqual('5');
+            createPage.getTextForRepeaterElement('result in memory', 0, 'result.timestamp').then(function (args) {
                 console.log(args);
             });
-            resultTimestampsecondRow.getText().then(function (args) {
+            createPage.getTextForRepeaterElement('result in memory', 1, 'result.timestamp').then(function (args) {
                 console.log(args);
             });
-            expect(CreatePage.repeater('result in memory', 0, 'result.first').getText()).toEqual('10');
-            expect(resultOperatorfirstRow.getText()).toEqual('/');
-            expect(resultSecondfirstRow.getText()).toEqual('2');
-            expect(resultValuefirstRow.getText()).toEqual('5');
-            expect(resultFirstsecondRow.getText()).toEqual('5');
-            expect(resultOperatorsecondRow.getText()).toEqual('+');
-            expect(resultsSecondsecondRow.getText()).toEqual('3');
-            expect(resultValuesecondRow.getText()).toEqual('8');
+            protractor_1.browser.waitForAngular();
+            expect(createPage.getTextForRepeaterElement('result in memory', 0, 'result.first')).toEqual('10');
+            expect(createPage.getTextForRepeaterElement('result in memory', 0, 'result.operator')).toEqual('/');
+            expect(createPage.getTextForRepeaterElement('result in memory', 0, 'result.second')).toEqual('2');
+            expect(createPage.getTextForRepeaterElement('result in memory', 0, 'result.value')).toEqual('5');
+            expect(createPage.getTextForRepeaterElement('result in memory', 1, 'result.first')).toEqual('5');
+            expect(createPage.getTextForRepeaterElement('result in memory', 1, 'result.operator')).toEqual('+');
+            expect(createPage.getTextForRepeaterElement('result in memory', 1, 'result.second')).toEqual('3');
+            expect(createPage.getTextForRepeaterElement('result in memory', 1, 'result.value')).toEqual('8');
         });
     });
 });
--- Test/Smoke/firstTest.spec.ts
+++ Test/Smoke/firstTest.spec.ts
@@ -1,21 +1,9 @@
 import { browser, element, by, By, $, $$, ExpectedConditions } from 'protractor';
-// import CreatePage = require('./Pages/firstTest.page.js');
+import CreatePage = require('./Pages/firstTest.page.js');
 
-// var createPage: CreatePage = new CreatePage();
-var CreatePage = require('./Pages/firstTest.page.js');
+var createPage: CreatePage = new CreatePage();
 
-describe('Test the protractor demo', () => {
-
-    let resultTimestampfirstRow = element(by.repeater('result in memory').row(0).column('result.timestamp'));
-    let resultTimestampsecondRow = element(by.repeater('result in memory').row(1).column('result.timestamp'));
-    // let resultFirstfirstRow = element(by.repeater('result in memory').row(0).column('result.first'));
-    let resultFirstsecondRow = element(by.repeater('result in memory').row(1).column('result.first'));
-    let resultOperatorfirstRow = element(by.repeater('result in memory').row(0).column('result.operator'));
-    let resultOperatorsecondRow = element(by.repeater('result in memory').row(1).column('result.operator'));
-    let resultSecondfirstRow = element(by.repeater('result in memory').row(0).column('result.second'));
-    let resultsSecondsecondRow = element(by.repeater('result in memory').row(1).column('result.second'));
-    let resultValuefirstRow = element(by.repeater('result in memory').row(0).column('result.value'));
-    let resultValuesecondRow = element(by.repeater('result in memory').row(1).column('result.value'));
+describe('Test the protractor Calculator', () => {
 
     beforeEach(function () {
 
@@ -28,100 +16,100 @@ describe('Test the protractor demo', () => {
     });
 
     describe('When it enters the page', () => {
-        xit('should be the correct page', () => {
+        it('it should be the correct page', () => {
             expect(browser.getCurrentUrl()).toBe('http://juliemr.github.io/protractor-demo/');
         });
-        xit('should check the title', () => {
+        it('it should check the title', () => {
             expect(browser.getTitle()).toBe('Super Calculator');
         });
     });
 
-    describe('When testing the calculator', () => {
+    describe('When calculating', () => {
 
         describe('When Using single calculations', () => {
-            it('should add two numbers, use addition and check the result value of the first line', () => {
-                CreatePage.enterFirstFieldValue('5');
-                CreatePage.chooseAddition();
-                CreatePage.enterSecondFieldValue('3');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('8'));
+            it('it should add two numbers, use addition and check the result value of the first line', () => {
+                createPage.enterFirstFieldValue('5');
+                createPage.chooseAddition();
+                createPage.enterSecondFieldValue('3');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('8');
             });
-            xit('should add two numbers,use division and check the result value of the first line', () => {
-                CreatePage.enterFirstFieldValue('10');
-                CreatePage.chooseDivision();
-                CreatePage.enterSecondFieldValue('2');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('5'));
+            it('it should add two numbers,use division and check the result value of the first line', () => {
+                createPage.enterFirstFieldValue('10');
+                createPage.chooseDivision();
+                createPage.enterSecondFieldValue('2');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('5');
             });
-            xit('should add two numbers,use multiplication and check the result value of the first line', () => {
-                CreatePage.enterFirstFieldValue('25');
-                CreatePage.chooseMultiplication();
-                CreatePage.enterSecondFieldValue('7');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('125'));
+            it('it should add two numbers,use multiplication and check the result value of the first line', () => {
+                createPage.enterFirstFieldValue('25');
+                createPage.chooseMultiplication();
+                createPage.enterSecondFieldValue('7');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('175');
             });
-            xit('should add two numbers,use subtraction and check the result value of the first line', () => {
-                CreatePage.enterFirstFieldValue('0');
-                CreatePage.chooseSubstraction();
-                CreatePage.enterSecondFieldValue('10');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('-10'));
+            it('it should add two numbers,use subtraction and check the result value of the first line', () => {
+                createPage.enterFirstFieldValue('0');
+                createPage.chooseSubstraction();
+                createPage.enterSecondFieldValue('10');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('-10');
             });
-            xit('should add two numbers,use modulo and check the result value of the first line', () => {
-                CreatePage.enterFirstFieldValue('5000');
-                CreatePage.chooseModulo();
-                CreatePage.enterSecondFieldValue('80');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('40'));
+            it('it should add two numbers,use modulo and check the result value of the first line', () => {
+                createPage.enterFirstFieldValue('5000');
+                createPage.chooseModulo();
+                createPage.enterSecondFieldValue('80');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('40');
             });
         });
 
         describe('When using multiple calculations', () => {
-            xit('should do different operations in one session and check the result for the latest', () => {
-                CreatePage.enterFirstFieldValue('5');
-                CreatePage.chooseAddition();
-                CreatePage.enterSecondFieldValue('3');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('8'));
-                CreatePage.enterFirstFieldValue('10');
-                CreatePage.chooseDivision();
-                CreatePage.enterSecondFieldValue('2');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('5'));
-                CreatePage.enterFirstFieldValue('10');
-                CreatePage.chooseMultiplication();
-                CreatePage.enterSecondFieldValue('2');
-                CreatePage.clickGO();
-                expect(CreatePage.getLatest().toEqual('20'));
+            it('it should do different operations in one session and check the result for the latest', () => {
+                createPage.enterFirstFieldValue('5');
+                createPage.chooseAddition();
+                createPage.enterSecondFieldValue('3');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('8');
+                createPage.enterFirstFieldValue('10');
+                createPage.chooseDivision();
+                createPage.enterSecondFieldValue('2');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('5');
+                createPage.enterFirstFieldValue('10');
+                createPage.chooseMultiplication();
+                createPage.enterSecondFieldValue('2');
+                createPage.clickGO();
+                expect(createPage.getLatest()).toEqual('20');
             });
         });
 
-
-        xit('should test values from  ngRepeat', () => {
-            CreatePage.enterFirstFieldValue('5');
-            CreatePage.chooseAddition();
-            CreatePage.enterSecondFieldValue('3');
-            CreatePage.clickGO();
-            expect(CreatePage.getLatest().toEqual('8'));
-            CreatePage.enterFirstFieldValue('10');
-            CreatePage.chooseDivision();
-            CreatePage.enterSecondFieldValue('2');
-            CreatePage.clickGO();
-            expect(CreatePage.getLatest().toEqual('5'));
-            resultTimestampfirstRow.getText().then((args) => {
+        it('it should test values from  ngRepeat', () => {
+            createPage.enterFirstFieldValue('5');
+            createPage.chooseAddition();
+            createPage.enterSecondFieldValue('3');
+            createPage.clickGO();
+            expect(createPage.getLatest()).toEqual('8');
+            createPage.enterFirstFieldValue('10');
+            createPage.chooseDivision();
+            createPage.enterSecondFieldValue('2');
+            createPage.clickGO();
+            expect(createPage.getLatest()).toEqual('5');
+            createPage.getTextForRepeaterElement('result in memory', 0, 'result.timestamp').then((args) => {
                 console.log(args);
             });
-            resultTimestampsecondRow.getText().then((args) => {
+            createPage.getTextForRepeaterElement('result in memory', 1, 'result.timestamp').then((args) => {
                 console.log(args);
             });
-            expect(CreatePage.repeater('result in memory',0,'result.first').getText()).toEqual('10');
-            expect(resultOperatorfirstRow.getText()).toEqual('/');
-            expect(resultSecondfirstRow.getText()).toEqual('2');
-            expect(resultValuefirstRow.getText()).toEqual('5');
-            expect(resultFirstsecondRow.getText()).toEqual('5');
-            expect(resultOperatorsecondRow.getText()).toEqual('+');
-            expect(resultsSecondsecondRow.getText()).toEqual('3');
-            expect(resultValuesecondRow.getText()).toEqual('8');
+            browser.waitForAngular();
+            expect(createPage.getTextForRepeaterElement('result in memory', 0, 'result.first')).toEqual('10');
+            expect(createPage.getTextForRepeaterElement('result in memory', 0, 'result.operator')).toEqual('/');
+            expect(createPage.getTextForRepeaterElement('result in memory', 0, 'result.second')).toEqual('2');
+            expect(createPage.getTextForRepeaterElement('result in memory', 0, 'result.value')).toEqual('5');
+            expect(createPage.getTextForRepeaterElement('result in memory', 1, 'result.first')).toEqual('5');
+            expect(createPage.getTextForRepeaterElement('result in memory', 1, 'result.operator')).toEqual('+');
+            expect(createPage.getTextForRepeaterElement('result in memory', 1, 'result.second')).toEqual('3');
+            expect(createPage.getTextForRepeaterElement('result in memory', 1, 'result.value')).toEqual('8');
         });
     });
 });
