11741f5b18f3601e9f79cede7a93e06184ab0d18
Mascarponee <mascarponee@gmail.com>
2017-03-14 09:00:53 +0100

    Added configs

--- .vscode/protractor.conf.ts
+++ .vscode/protractor.conf.ts
@@ -20,4 +20,16 @@ export let config: Config = {
     showColors: true,
     defaultTimeoutInterval: 30000
   }
+  
+   // When navigating to a new page using browser.get, Protractor waits for the page to be loaded and the new URL to appear before continuing.
+    // getPageTimeout: 60000,
+
+    // Before performing any action, Protractor asks Angular to wait until the page is synchronized. This means that all timeouts and http requests are finished. If your application continuously polls $timeout or $http, it will never be registered as completely loaded. You should use the $interval service (interval.js) for anything that polls continuously
+    // allScriptsTimeout: 60000,
+  
+// onPrepare: function () {
+        //browser.driver.manage().window().maximize();
+        // browser.driver.manage().window().setSize(1600, 1200);
+  
+// baseUrl: "http://race-test/",
 };
